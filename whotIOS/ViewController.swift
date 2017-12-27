5d07cd316c73717b54e41f910b110b2ceb9c4516
Kevin <quickey123@gmail.com>
2017-12-27 13:25:00 +0800

    Initial Commit

---
+++ whotIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  whotIOS
+//
+//  Created by Sapido on 2017/12/27.
+//  Copyright © 2017年 Whot inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
