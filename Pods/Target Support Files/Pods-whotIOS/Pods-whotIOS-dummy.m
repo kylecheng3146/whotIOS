80d8c8b1a752347fd93e7f5b6592ca0fcc669755
Kevin <quickey123@gmail.com>
2018-01-23 15:24:32 +0800

    import cocoapod moya rxswift

---
+++ Pods/Target Support Files/Pods-whotIOS/Pods-whotIOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_whotIOS : NSObject
+@end
+@implementation PodsDummy_Pods_whotIOS
+@end
---
+++ Pods/Target Support Files/Pods-whotIOSUITests/Pods-whotIOSUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_whotIOSUITests : NSObject
+@end
+@implementation PodsDummy_Pods_whotIOSUITests
+@end
